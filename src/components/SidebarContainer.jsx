879ebd34674f9e42beff0120bcef8c05f362cebe
yumsha <rizagapakel30@gmail.com>
2026-04-19 03:19:35 +0700

    feat: added POKJA btn on sidebar for user role KESISWAAN

--- src/components/SidebarContainer.jsx
+++ src/components/SidebarContainer.jsx
@@ -263,6 +263,24 @@ export default function SidebarContainer() {
         </a>
       ): null}
 
+      {isKesiswaan && (
+        <a
+          href="/dashboard/pokja"
+          data-astro-prefetch
+          className={getLinkClass("/dashboard/pokja", false)}
+        >
+          <svg className="w-5 h-5" fill="none" stroke="currentColor" viewBox="0 0 24 24">
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth="2"
+              d="M9 5H7a2 2 0 00-2 2v12a2 2 0 002 2h10a2 2 0 002-2V7a2 2 0 00-2-2h-2M9 5a2 2 0 002 2h2a2 2 0 002-2M9 5a2 2 0 012-2h2a2 2 0 012 2m-6 9l2 2 4-4"
+            />
+          </svg>
+           Manajemen Pokja
+        </a>
+      )}
+
       {/* ---- ADMIN ---- */}
 
       {isAdmin && (
@@ -276,7 +294,7 @@ export default function SidebarContainer() {
         </a>
       )}
 
-      {isAdmin && (
+      { isAdmin && (
         <a
           href="/dashboard/importOrangTua"
           data-astro-prefetch
