130d564bbc7bea4c0c341698bf35bc37f21922e5
Dipsii1 <nadifaryastia@gmail.com>
2026-08-31 14:57:54 +0700

    feat: update config astro and move photos to public

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,14 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
+import node from '@astrojs/node';
 
 // https://astro.build/config
 export default defineConfig({
+  output: 'server',
+  adapter: node({
+    mode: 'standalone',
+  }),
+  site: 'http://10.1.128.31',
   integrations: [react()],
 });
